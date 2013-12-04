5c0de52ad78f399243296bc60addb553ffb52139
Di Huang <dhuang@linkedin.com>
2013-12-03 22:51:11 -0800

    Add a solution

---
+++ streamAdventure/transform.js
@@ -0,0 +1,9 @@
+var through = require('through');
+
+function write(buf) {
+  this.queue(buf.toString().toUpperCase());
+}
+
+var tr = through(write);
+
+process.stdin.pipe(tr).pipe(process.stdout);
